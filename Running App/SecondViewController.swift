c266b0930b4c1835be892e3b3678d0f5ca8cb4b8
ar4757 <ar4757@gmail.com>
2019-01-15 16:14:13 -0500

    Initial Commit

---
+++ Running App/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Running App
+//
+//  Created by Andrew Ratz on 1/15/19.
+//  Copyright © 2019 Andrew Ratz. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
